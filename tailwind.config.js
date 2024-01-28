1b0f034f4086a2cab484492a30bd9355e786bc1f
ashley <ashleyhsueh33@gmail.com>
2024-01-28 14:06:15 +0800

    Remove unused dependencies and add "daisyui" plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx,'node_modules/flowbite-react/lib/esm/**/*.js'}",
-  ],
-  // make sure to safelist these classes when using purge
-  safelist: [
-    "w-64",
-    "w-1/2",
-    "rounded-l-lg",
-    "rounded-r-lg",
-    "bg-gray-200",
-    "grid-cols-4",
-    "grid-cols-7",
-    "h-6",
-    "leading-6",
-    "h-9",
-    "leading-9",
-    "shadow-lg",
-  ],
-
-  // enable dark mode via class strategy
-  darkMode: "class",
-
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,"],
   theme: {
     extend: {},
   },
-  plugins: [import("flowbite/plugin")],
+  plugins: [import("daisyui")],
 };
