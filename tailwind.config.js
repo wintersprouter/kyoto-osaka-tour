07b1213765a87599dc3c94c25d6cdf7f90f2f875
ashley <ashleyhsueh33@gmail.com>
2024-01-28 13:00:57 +0800

    Add flowbite and flowbite-react dependencies, update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,'node_modules/flowbite-react/lib/esm/**/*.js'}",
+  ],
+  // make sure to safelist these classes when using purge
+  safelist: [
+    "w-64",
+    "w-1/2",
+    "rounded-l-lg",
+    "rounded-r-lg",
+    "bg-gray-200",
+    "grid-cols-4",
+    "grid-cols-7",
+    "h-6",
+    "leading-6",
+    "h-9",
+    "leading-9",
+    "shadow-lg",
+  ],
+
+  // enable dark mode via class strategy
+  darkMode: "class",
+
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [import("flowbite/plugin")],
 };
