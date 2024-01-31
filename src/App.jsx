e1f50e7c9dd5c7b28b7f808c4fa633298f561ecf
ashley <ashleyhsueh33@gmail.com>
2024-01-31 17:05:15 +0800

    Add @coreui/icons, @coreui/icons-react, and react-feather dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
+import { Clock, DollarSign, MapPin } from "react-feather";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Tabs from "./components/Tabs";
+import ITINERARIES from "./data.json";
 const days = [
   {
     id: "1",
@@ -34,47 +36,119 @@ function App() {
         selectedDay={selectedDay}
         setSelectedDay={setSelectedDay}
       />
-      <section className='my-5 '>
+      <section className='my-5'>
         <ul className='timeline timeline-vertical timeline-compact'>
-          <li>
-            <hr />
-            <div className='timeline-start'>上午5點</div>
-            <div className='timeline-middle'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 20 20'
-                fill='currentColor'
-                className='w-5 h-5 text-primary'
-              >
-                <path
-                  fillRule='evenodd'
-                  d='M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z'
-                  clipRule='evenodd'
-                />
-              </svg>
-            </div>
-            <div className='timeline-end'>
-              <div className='container flex flex-col gap-4'>
-                <div className='card card-compact bg-base-100 shadow-2xl'>
-                  <div className='card-body'>
-                    <h2 className='card-title'>Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className='card-actions justify-end'></div>
+          {ITINERARIES.map((item, index) => {
+            return (
+              <li key={`${item.attraction.name}${index}`}>
+                <hr />
+                <div className='timeline-start'>
+                  {item.transportation.departure_time}
+                </div>
+                <div className='timeline-middle'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    viewBox='0 0 20 20'
+                    fill='currentColor'
+                    className='w-5 h-5 text-primary'
+                  >
+                    <path
+                      fillRule='evenodd'
+                      d='M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z'
+                      clipRule='evenodd'
+                    />
+                  </svg>
+                </div>
+                <div className='timeline-end'>
+                  <div className='container flex flex-col gap-4'>
+                    <div className='card card-compact card-side bg-base-100 shadow-2xl'>
+                      {item.attraction.image_url && (
+                        <figure>
+                          <img
+                            src={item.attraction.image_url}
+                            alt={item.attraction.name}
+                            className='rounded-lg bg-cover h-full md:max-w-5'
+                          />
+                        </figure>
+                      )}
+                      <div className='card-body'>
+                        <h2 className='card-title'>{item.attraction.name}</h2>
+                        <p className='text-left'>
+                          {item.attraction.description}
+                        </p>
+                        <div className='card-actions justify-start col'>
+                          <button
+                            className='btn'
+                            disabled={item.attraction.location_url.length === 0}
+                            onClick={() => {
+                              if (item.attraction.location_url.length === 0)
+                                return;
+                              window.open(
+                                item.attraction.location_url,
+                                "_blank"
+                              );
+                            }}
+                          >
+                            <MapPin />
+                            {item.attraction.location}
+                          </button>
+                          {item.attraction.fee.length > 0 && (
+                            <div className='badge badge-outline badge-lg border-primary'>
+                              <span className='mr-2'>
+                                <DollarSign size={18} />
+                              </span>
+                              {item.attraction.fee}
+                            </div>
+                          )}
+                          {item.attraction.opening_time.length > 0 && (
+                            <div className='badge badge-outline badge-lg badge-secondary'>
+                              <span className='mr-2'>
+                                <Clock size={18} />
+                              </span>
+
+                              {item.attraction.opening_time}
+                            </div>
+                          )}
+                        </div>
+                      </div>
+                    </div>
+                    <div
+                      className='card card-compact bg-secondary
+                  text-primary-content shadow-xl'
+                    >
+                      <div className='card-body'>
+                        <h2 className='card-title'>
+                          {item.transportation.type}
+                        </h2>
+                        <p className='text-left'>
+                          {item.transportation.description}
+                        </p>
+                      </div>
+                      <div className='card-actions justify-start'>
+                        {item.transportation.fee.length > 0 && (
+                          <div className='badge badge-accent badge-lg'>
+                            <span className='mr-2'>
+                              <DollarSign size={18} />
+                            </span>
+                            {item.transportation.fee}
+                          </div>
+                        )}
+                        {item.transportation.duration.length > 0 && (
+                          <div className='badge badge-ghost badge-lg '>
+                            <span className='mr-2'>
+                              <Clock size={18} />
+                            </span>
+                            {item.transportation.duration}
+                          </div>
+                        )}
+                      </div>
+                    </div>
                   </div>
                 </div>
-                <div
-                  className='card card-compact bg-secondary
-                text-primary-content shadow-xl'
-                >
-                  <div className='card-body'>
-                    <h2 className='card-title'>Card title!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <hr />
-          </li>
+                <hr />
+              </li>
+            );
+          })}
         </ul>
       </section>
     </>
