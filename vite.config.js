d3898e210dc36abb72eb06d5c66aab4ea560d703
ashley <ashleyhsueh33@gmail.com>
2024-02-01 10:13:42 +0800

    Update file paths and base URL

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import react from "@vitejs/plugin-react";
+import process from "process";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/kyoto-osaka-tour/",
+  base: process.env.NODE_ENV === "production" ? "/kyoto-osaka-tour/" : "/",
 });
